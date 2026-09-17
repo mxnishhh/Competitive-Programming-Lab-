f0c5ad561dbe828d8d44f7e241aa11bdde577c6b
manish <mr.manishkrm@gmail.com>
2026-09-17 10:52:58 +0530

    Update

--- Sorting/1.Bubble.py
+++
@@ -1,8 +0,0 @@
-arr = list(map(int, input("Enter elements: ").split()))
-
-for i in range(len(arr)):
-    for j in range(0, len(arr) - i - 1):
-        if arr[j] > arr[j + 1]:
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-print("Sorted array:", arr)
